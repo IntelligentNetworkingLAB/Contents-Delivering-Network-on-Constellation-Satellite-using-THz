ae69719482038bcd2bfd0f77195092ddefa4524b
minstone_98 <50548788+alstjrdld1@users.noreply.github.com>
2022-11-03 19:19:11 +0900

    Add files via upload

---
+++ MyConstants.py
@@ -0,0 +1,22 @@
+# Scientific Params
+C = 3e+8 # m/s
+BOLTZMAN = 1e-23 # J/K
+ENVTEMP = 1000 # 500 K 
+
+# Constraints
+PMAX = 10
+LMAX = 0.002
+TOTAL_LMAX = 0.1
+
+# Antenna Configuration
+ANTENNA_GAIN = 60
+ANTENNA_EFFICIENCY = 0.1
+
+ADDITIONAL_NOISE_Receiver = 0.1
+
+# Data
+DATA_AMOUNT = 0.3e+10 # 30 G 
+BANDWIDTH = 1e+11 # 0.1THz
+
+# For Test
+FIXED_DISTANCE = 5e+4 # m
